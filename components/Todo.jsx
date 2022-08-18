b8c3f34a077be88a91f6248d0a8ace363e110479
Pantagon <pichayoothunchainao@gmail.com>
2022-08-18 16:28:30 +0700

    rest

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import {
   IconCheck,
   IconTrash,
@@ -7,5 +8,33 @@ import {
 } from "@tabler/icons";
 
 export default function Todo(props) {
-  return <div>your code here...</div>;
+  const [hide, setHide] = useState({ display: "none" });
+
+  return (
+    <div
+      onMouseOver={() => setHide({ display: "" })}
+      className="border-bottom p-1 py-2 fs-2 d-flex gap-2"
+      onMouseOut={() => setHide({ display: "none" })}
+    >
+      <span
+        className="me-auto"
+        style={{ textDecoration: props.completed ? "line-through" : "" }}
+      >
+        {props.title}
+      </span>
+
+      <button style={hide} onClick={props.onMark} className="btn btn-success">
+        <IconCheck />
+      </button>
+      <button style={hide} className="btn btn-secondary">
+        <IconArrowUp />
+      </button>
+      <button style={hide} className="btn btn-secondary">
+        <IconArrowDown />
+      </button>
+      <button style={hide} onClick={props.ondelete} className="btn btn-danger">
+        <IconTrash />
+      </button>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { Component, useEffect, useState } from "react";
 import Todo from "../components/Todo";
+// import Todo from "../components/Todo";
 import {
   IconCheck,
   IconTrash,
@@ -8,13 +9,51 @@ import {
 } from "@tabler/icons";
 
 export default function Home() {
-  const deleteTodo = (idx) => {};
+  const [todo, setTodo] = useState([]);
 
-  const markTodo = (idx) => {};
+  const deleteTodo = (idx) => {
+    setTodo(todo.filter((_, i) => i !== idx));
+  };
+
+  const markTodo = (idx) => {
+    setTodo(
+      todo.map((v, i) => {
+        return {
+          title: v.title,
+          completed: idx === i ? !v.completed : v.completed,
+        };
+      })
+    );
+  };
+
+  const onKeyUpHandler = (e) => {
+    if (e.key !== "Enter") return;
+    if (e.target.value === "") {
+      alert("asdasdasd");
+      return;
+    }
+    const newTodo = [{ title: e.target.value, completed: false }, ...todo];
+    setTodo(newTodo);
+  };
 
   const moveUp = (idx) => {};
 
   const moveDown = (idx) => {};
+
+  const [output, setOutput] = useState([]);
+  useEffect(() => {
+    setOutput(
+      todo.map((c, i) => (
+        <Todo
+          title={c.title}
+          completed={c.completed}
+          key={i}
+          onMark={() => markTodo(i)}
+          ondelete={() => deleteTodo(i)}
+        />
+      ))
+    );
+  }, [todo]);
 
   return (
     <div>
@@ -28,40 +67,40 @@ export default function Home() {
         <input
           className="form-control mb-1 fs-4"
           placeholder="insert todo here..."
+          onKeyUp={onKeyUpHandler}
         />
         {/* Todos */}
-        {/* Example 1 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo</span>
-        </div>
+        {/* { Example 1 } */}
+
+        {output}
         {/* Example 2 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo with buttons</span>
+        {/*
+          <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
+            <span className="me-auto">Todo with buttons</span>
 
-          <button className="btn btn-success">
-            <IconCheck />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowUp />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowDown />
-          </button>
-          <button className="btn btn-danger">
-            <IconTrash />
-          </button>
-        </div>
-
+            <button className="btn btn-success">
+              <IconCheck />
+            </button>
+            <button className="btn btn-secondary">
+              <IconArrowUp />
+            </button>
+            <button className="btn btn-secondary">
+              <IconArrowDown />
+            </button>
+            <button className="btn btn-danger">
+              <IconTrash />
+            </button>
+          </div>
+      */}
         {/* summary section */}
         <p className="text-center fs-4">
           <span className="text-primary">All (2) </span>
           <span className="text-warning">Pending (2) </span>
           <span className="text-success">Completed (0)</span>
         </p>
-
         {/* Made by section */}
         <p className="text-center mt-3 text-muted fst-italic">
-          made by Chayanin Suatap 12345679
+          made by Pichayoot Hunchainao 640610653
         </p>
       </div>
     </div>
